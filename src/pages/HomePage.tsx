3512162de04cdcc25d99af4099811c8db2a1a70a
tukaianirban <mukherjee.anirban86@gmail.com>
2025-02-03 00:35:24 +0530

    impl: added section for joining us in home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,8 +2,32 @@ import { useEffect } from 'react';
 import { Facebook, Instagram, Youtube, Mail } from 'lucide-react';
 import { Footer } from '../components/Footer';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Home, Calendar, Users, Info } from 'lucide-react';
 
 function HomePage() {
+
+  const [activeSection, setActiveSection] = useState('home');
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentSection = sections.find(section => {
+        const element = document.getElementById(section.id);
+        if (element) {
+          const rect = element.getBoundingClientRect();
+          return rect.top <= window.innerHeight / 2 && rect.bottom >= window.innerHeight / 2;
+        }
+        return false;
+      });
+      if (currentSection) {
+        setActiveSection(currentSection.id);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   useEffect(() => {
     const observerOptions = {
       root: null,
@@ -36,6 +60,13 @@ function HomePage() {
       link: "/events/poila-boishakh-2025"
     },
     {
+      title: "Holi 2025",
+      date: "Mar 14, 2025",
+      description: "Holi Celebration",
+      image: "/images/holi-colors-hands.jpg",
+      link: "/events/holi-2025"
+    },
+    {
       title: "Durga Puja 2025",
       date: "Sep 28 - Oct 02, 2025",
       description: "Annual Durga Puja Festival",
@@ -43,22 +74,49 @@ function HomePage() {
       link: "/events/durga-puja-2025"
     },
     {
-      title: "Alpona competition",
-      date: "June 02, 2025",
-      description: "Drawing competition for kids",
+      title: "Sit and Draw Competition",
+      date: "(tbd), 2025",
+      description: "Drawing workshop for kids",
       image: "/images/hand-alpona.jpg",
-      link: "/events/alpona-competition"
+      link: "/events/sit-and-draw-2025"
+    },
+    {
+      title: "Diwali 2025",
+      date: "(tbd), 2025",
+      description: "Diwali 2025 Celebration",
+      image: "/images/candles-among-diwali-designs.jpg",
+      link: "/events/diwali-2025"
     }
+  ];
+
+  const sections = [
+    { id: 'home', icon: <Home size={24} /> },
+    { id: 'events', icon: <Calendar size={24} /> },
+    { id: 'membership', icon: <Users size={24} /> },
+    { id: 'organization', icon: <Info size={24} /> }
   ];
 
   return (
     <div className="w-full min-h-screen bg-background_color">
+    
+      {/* Overlay Navigation */}
+      <div className="fixed bottom-8 left-1/2 transform -translate-x-1/2 bg-gray-700/50 backdrop-blur-md rounded-full p-4 flex space-x-4 z-50">
+        {sections.map(section => (
+          <a
+            key={section.id}
+            href={`#${section.id}`}
+            className={`p-2 rounded-full text-white transition-opacity duration-300 ${activeSection === section.id ? 'opacity-100' : 'opacity-50'}`}
+          >
+            {section.icon}
+          </a>
+        ))}
+      </div>
 
       {/* Hero Section */}
       <section id="home" className="h-screen relative">
         <div className="absolute inset-0">
           <img 
-            src="/images/hero-image2.jpg"
+            src="/images/candles-among-diwali-designs.jpg"
             alt="Bengali Culture" 
             className="absolute inset-0 w-full h-full object-cover object-center" 
           />
@@ -86,47 +144,84 @@ function HomePage() {
       </section>
 
       {/* Events Section */}
-      <section className="py-20 px-8 md:px-16 lg:px-24 bg-gray-50" id="events">
-        <div className="animate-on-scroll opacity-0">
-          <h2 className="text-4xl font-bold mb-12 text-center">Upcoming Events</h2>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-            {events.map((event, index) => (
-              <div 
-                key={index}
-                className="group relative overflow-hidden rounded-lg shadow-lg transform transition-all duration-300 hover:-translate-y-2 hover:shadow-xl"
-              >
-                <div className="relative h-64">
-                  <img 
-                    src={event.image} 
-                    alt={event.title}
-                    className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-110"
-                  />
-                  <div className="absolute inset-0 bg-black/50 opacity-0 group-hover:opacity-100 transition-opacity duration-300" />
-                  <Link to={event.link}>
-                    <div className="absolute inset-0 p-6 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex flex-col justify-end">
-                      <h3 className="text-xl font-bold mb-2">{event.title}</h3>
-                      <p className="text-sm mb-2">{event.date}</p>
-                      <p>{event.description}</p>
-                    </div>
-                  </Link>
-                </div>
+      <section className="w-full min-h-screen bg-gray-50" id="events">
+        <div className="h-full w-full animate-on-scroll opacity-0 flex flex-col justify-center px-8 md:px-16 lg:px-24">
+          <h2 className="text-4xl font-bold mb-12 text-center mt-12">Upcoming Events</h2>
+          <div className="h-full w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {
+          events.map((event, index) => (
+          <div 
+            key={index}
+            className="group relative overflow-hidden rounded-lg shadow-lg transform transition-all duration-300 hover:-translate-y-2 hover:shadow-xl flex items-center justify-center">
+            
+            <div className="relative h-full w-full">
+              <img 
+                src={event.image} 
+                alt={event.title}
+                className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-110" />
+                
+                <div className="absolute inset-0 bg-black/50 opacity-0 group-hover:opacity-100 transition-opacity duration-300" />
+
+                <Link to={event.link}>
+                  <div className="absolute inset-0 p-6 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex flex-col justify-end">
+                    <h3 className="text-xl font-bold mb-2">{event.title}</h3>
+                    <p className="text-sm mb-2">{event.date}</p>
+                    <p>{event.description}</p>
+                  </div>
+                </Link>
               </div>
-            ))}
+            </div>
+          ))
+        }
           </div>
         </div>
       </section>
 
+      {/* Membership Callout Section */}
+      <section className="w-full min-h-screen relative" id="membership">
+        <div className="absolute inset-0">
+          <img 
+            src="/images/fireworkds-at-side-of-hill.jpg"
+            alt="Bengali Culture" 
+            className="absolute inset-0 w-full h-full object-cover object-center" 
+          />
+          <div className="absolute inset-0 bg-gradient-to-l from-black/80 to-transparent" />
+        </div>
+
+        <div className="min-h-screen flex items-center justify-end px-8 md:px-16 lg:px-24 animate-on-scroll opacity-0">
+            <div className="max-w-2xl text-white animate-hero-fade-in opacity-0 text-center flex flex-col items-center justify-center h-full">
+              <p className="mb-8 text-gray-100">
+                Sanskriti e.V. is a multi-cultural club that welcomes people from all cultures, backgrounds, and interest groups. 
+                We are dedicated to bringing together individuals who share a passion for celebrating Indian festivals and promoting cultural exchange. 
+                <br /><br />
+                By joining our club, you will become part of a vibrant community that values diversity and togetherness.
+              </p>
+              <div className="flex space-x-6 justify-end my-8">
+                <a 
+                  href="mailto:info@sanskriti-hamburg.de" target='_blank' rel='noreferrer'
+                  className="bg-blue-600 text-white px-8 py-3 rounded-full font-semibold hover:bg-blue-700 transition-colors duration-300">
+                  Become a Member
+                </a>
+              </div>
+            </div>
+        </div>
+        
+      </section>
+
       {/* Organization Section */}
-      <section className="py-20 px-8 md:px-16 lg:px-24 bg-white" id="organization">
+      <section className="w-full min-h-screen py-20 px-8 md:px-16 lg:px-24 bg-white" id="organization">
         <div className="max-w-7xl mx-auto animate-on-scroll opacity-0">
-          <h2 className="text-4xl font-bold mb-12 text-center">Our Organization</h2>
+          <h2 className="text-4xl font-bold mb-24 text-center">Our Organization</h2>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
-            <div className="relative h-[400px] overflow-hidden rounded-lg">
-              <img 
+            <div>
+              <div className="relative h-[400px] overflow-hidden rounded-lg">
+                <img 
                 src="/images/vorstand_ladies_2.jpg"
                 alt="Cultural Performance"
                 className="object-cover rounded-lg"
-              />
+                />
+              </div>
+              <p className="text-center text-gray-600 text-xs mt-2">Members of the Board of Sanskriti e.V.</p>
             </div>
             <div className="flex flex-col space-y-6">
               <div className="prose lg:prose-xl">
@@ -135,13 +230,12 @@ function HomePage() {
                   Our mission is to create a vibrant community that celebrates our rich heritage while fostering cultural exchange and understanding.
                 </p>
               </div>
-              <div className="flex flex-col sm:flex-row gap-4 items-center">
+              <div className="flex flex-col sm:flex-row gap-4 py-12 justify-center">
                 <form action="https://www.paypal.com/donate" method="post" target="_blank">
                   <button 
                     type="submit"
-                    className="w-full sm:w-auto bg-[#0070BA] text-white px-8 py-3 rounded-full font-semibold hover:bg-[#003087] transition-colors duration-300 flex items-center justify-center space-x-2"
-                  >
-                    <span>Donate with PayPal</span>
+                    className="w-full sm:w-auto bg-[#0070BA] text-white px-8 py-3 rounded-full font-semibold hover:bg-[#003087] transition-colors duration-300 flex items-center justify-center space-x-2">
+                    <span>Donate to the Club</span>
                   </button>
                 </form>
                 <a 
@@ -149,7 +243,7 @@ function HomePage() {
                   className="w-full sm:w-auto bg-emerald-600 text-white px-8 py-3 rounded-full font-semibold hover:bg-emerald-700 transition-colors duration-300 flex items-center justify-center space-x-2"
                 >
                   <Mail className="w-5 h-5" />
-                  <span>Contact Us</span>
+                  <span>Join Us</span>
                 </a>
               </div>
             </div>
